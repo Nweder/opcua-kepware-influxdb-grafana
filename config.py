8d4e47026df3125e6a2068d66eee1770fce072b2
Mohamad Nweder <m.neweder@gmail.com>
2026-03-24 20:08:53 +0100

    added comments to future implementation with real Kepware

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ import os
 
 load_dotenv()
 
-
+# we change it to VBG kepware server later ex :"https://IP-ADRESS:39320/iotgateway"
 KEPWARE_BASE_URL = "http://localhost:5000"
 
+# We need also to change tages later, these are just examples
 TAG_IDS = [
     "Channel1.Device1.temperature",
     "Channel1.Device1.run_signal",
@@ -14,6 +15,9 @@ TAG_IDS = [
     "Channel1.Device1.alarm_code"
 ]
 
+
+
+# We don't need any more longer mock_kepware.py file, we can just change the URL to point to the real Kepware server and update the tag IDs as needed. The rest of the code will work the same way.
 
 INFLUX_URL = "https://us-east-1-1.aws.cloud2.influxdata.com"
 INFLUX_TOKEN = os.getenv("INFLUX_TOKEN")
@@ -24,4 +28,5 @@ POLL_INTERVAL = 1
 JSON_OUTPUT_FILE = "data.jsonl"
 WRITE_TO_INFLUX = True
 
+# just to cleary that server working and token is loaded, we can remove this later
 print("Token loaded:", INFLUX_TOKEN is not None)
